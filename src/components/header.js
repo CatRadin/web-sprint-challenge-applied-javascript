cf386c20ea32e09c7480548ed50e6e441fd905a3
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-01-15 12:12:55 -0600

    changed a name

--- src/components/header.js
+++ src/components/header.js
@@ -49,7 +49,7 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-  document.querySelector(selector).append(Header('Catherine Times', 'September 10 1991', '73'));
+  document.querySelector(selector).append(Header('The Catherine Times', 'September 10 1991', '73'));
 
 }
 
